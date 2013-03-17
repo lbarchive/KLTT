9c5f04d45e0b9996aad162dc244a1881f73d25ac
Yu-Jie Lin <livibetter@gmail.com>
2013-03-18 06:56:06 +0800

    fix options doesn't get updated in background after options are saved

--- background.js
+++ background.js
@@ -4,23 +4,40 @@ var creating_tab = false;
 
 
 // Grab data from sync.
-var select_new_created;
-var close_second_tab;
-var auto_pin;
-var open_custom_page;
-var custom_page_url;
-chrome.storage.sync.get(["select_new_created", "close_second_tab", "auto_pin", "open_custom_page", "custom_page_url"], 
-	function(items) {
-		select_new_created = items['select_new_created'];
-		close_second_tab = items['close_second_tab'];
-		auto_pin = items['auto_pin'];
-		open_custom_page = items['open_custom_page'];
-		custom_page_url = items['custom_page_url'];
-	}
+var options = {};
+chrome.storage.sync.get(
+  ["select_new_created", "close_second_tab", "auto_pin", "open_custom_page", "custom_page_url"], 
+  update_options
 );
 
 
 // Functions.
+function update_options(items) {
+  for (key in items) {
+    value = items[key];
+    if (typeof(value) == 'object') {
+      // from chrome.storage.onChanged event.
+      value = value.newValue;
+    } else {
+      value = items[key];
+    }
+    switch (key) {
+      case "select_new_created":
+      case "close_second_tab":
+      case "auto_pin":
+      case "open_custom_page":
+        options[key] = !!value;
+        break;
+      case "custom_page_url":
+        options[key] = value || '';
+        break;
+      default:
+        console.error('Unknown option ' + key + ': ' + value);
+        break;
+    }
+  }
+}
+
 function keep_two() {
 	chrome.windows.getAll({populate: true}, function(windows){
 		var tab_count = 0;
@@ -33,12 +50,12 @@ function keep_two() {
 			creating_tab = true;
 			
 			var create_options = {
-				selected: select_new_created,
-				pinned: (auto_pin && !w.tabs[0].pinned)
+				selected: options.select_new_created,
+				pinned: (options.auto_pin && !w.tabs[0].pinned)
 			};
 			
-			if (open_custom_page == 1)
-				create_options['url'] = custom_page_url;
+			if (options.open_custom_page)
+				create_options['url'] = options.custom_page_url;
 				
 			chrome.tabs.create(create_options, function(tab){
 				// save this tab id in case of Close Tabs to The Right.
@@ -86,9 +103,10 @@ chrome.tabs.onCreated.addListener(function(tab) {
 	if (tab.id != reopen_tab_id)
 		reopen_tab_id = null;
 	
-	// ...What?
-	close_second_tab = (close_second_tab == 1);
-	
-	if (close_second_tab)
+	if (options.close_second_tab)
 		close_second(tab);
 });
+
+chrome.storage.onChanged.addListener(function(changes, areaName) {
+  update_options(changes);
+}); 
