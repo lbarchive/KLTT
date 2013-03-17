7ca79aa852df7de39aefc765d7f424f0a8dd9df1
Yu-Jie Lin <livibetter@gmail.com>
2013-03-18 07:35:22 +0800

    Version 0.6

--- options.js
+++ options.js
@@ -1,6 +1,5 @@
 ﻿// Saves options to chrome sync storage.
 function saveOps() {
-	
 	chrome.storage.sync.set({
 		"select_new_created": document.getElementById("select_new_created").checked,
 		"close_second_tab": document.getElementById("close_second_tab").checked,
@@ -19,17 +18,15 @@ function saveOps() {
 
 // Restores select box state to saved value from chrome sync storage.
 function loadOps() {
-	chrome.storage.sync.get(["select_new_created", "close_second_tab", "auto_pin", "open_custom_page", "custom_page_url"], 
+	chrome.storage.sync.get(
+    ["select_new_created", "close_second_tab", "auto_pin", "open_custom_page", "custom_page_url"], 
 		function(items) {
-			
 			document.getElementById("select_new_created").checked = !!items["select_new_created"];
 			document.getElementById("close_second_tab").checked = !!items["close_second_tab"];
 			document.getElementById("auto_pin").checked = !!items["auto_pin"];
 			document.getElementById("open_custom_page").checked = !!items["open_custom_page"];
 			document.getElementById("custom_page_url").value = items["custom_page_url"] || '';
-			
-		}
-	);
+	});
 }
 
 // Add the events to load/save from this page.
